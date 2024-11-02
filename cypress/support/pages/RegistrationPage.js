20f326ad1a1bfcee6a5ea83f351e64a90dfb0f86
jagadishmoharana <170645587+jagadishmoharana@users.noreply.github.com>
2024-11-02 11:15:40 +0530

    first commit

--- cypress/support/pages/RegistrationPage.js
+++ cypress/support/pages/RegistrationPage.js
@@ -8,30 +8,31 @@ class RegistraionPage{
         privacyPolicy:"[name='agree']",
         continue:"[type='submit']"
     }
+    // Navigate to the url
     openUrl(){
         cy.visit('https://demo.opencart.com/en-gb?route=account/register')
     }
-
+    // Enter your first name
     enterFirstName(FName){
         cy.get(this.webLocators.firstName).type(FName)
     }
-
+    // Enter your last name
     enterLastName(LName){
         cy.get(this.webLocators.lastName).type(LName)
     }
-
+    // Enter your email
     enterEmail(EMail){
         cy.get(this.webLocators.eMail).type(EMail)
     }
-
+    // Enter your password
     enterPassword(Password){
         cy.get(this.webLocators.password).type(Password)
     }
-
+    // Accept privacy policy
     checkPrivacyPolicy(){
         cy.get(this.webLocators.privacyPolicy).check()
     }
-
+    // Click on continue to submit registration form
     clickOnContinue(){
         cy.get(this.webLocators.continue).click()
     }
